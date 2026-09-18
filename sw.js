ccdf1ea6711f51db458074df4604eecab0befd03
ngsmetadvisor <ngsmetadvisor@gmail.com>
2026-09-18 15:37:37 -0600

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,28 @@
+const CACHE = 'longexp-v5';
+const FILES = ['./camera.html', './manifest.webmanifest'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+// Network first so updates arrive when online, cached copy when offline
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    fetch(e.request)
+      .then(r => {
+        const copy = r.clone();
+        caches.open(CACHE).then(c => c.put(e.request, copy));
+        return r;
+      })
+      .catch(() => caches.match(e.request, { ignoreSearch: true }))
+  );
+});
